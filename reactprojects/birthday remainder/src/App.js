93096bf5ffdd94f98f17c7e710cf13280e1bb2eb
shivaEngagebay <shiva@engagebay.co>
2022-09-27 17:56:56 +0530

    react birthday remainder

---
+++ reactprojects/birthday remainder/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const FirstApp = () => {
+    return(
+        <div>
+            <h1>React JS</h1>
+            <ul>
+                <li>React js is developed by Facebook engineers</li>
+                <li>React Js is used to develop UI</li>
+                <li>Learning react is fun</li>
+            </ul>
+        </div>
+    )
+}
+
+export default FirstApp;
---
+++ reactprojects/birthday remainder/src/birthdayRemainder/birthdayApp.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './birthdayList'
+import "./birthdayStyle.css"
+
+function BirthDayApp() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
+}
+
+export default BirthDayApp;
